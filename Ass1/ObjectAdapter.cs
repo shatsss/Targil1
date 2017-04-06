1f841db515a2cd39adda1ac9a6fa19e1a8695c5d
אלון שץ <alonshats49@gmail.com>
2017-04-06 15:20:28 +0300

    object adapter

---
+++ Ass1/ObjectAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchAlgorithmsLib;
+using MazeLib;
+using MazeGeneratorLib;
+
+namespace Ass1
+{
+    public class ObjectAdapter<T> : ISearchable<T>
+    {
+        private Maze maze;
+        public ObjectAdapter()
+        {
+            this.maze = new Maze();
+        }
+        public List<State<T>> getAllPossibleStates(State<T> s)
+        {
+            return null;
+        }
+        public State<T> getGoalState()
+        {
+            return null;
+        }
+        public State<T> getInitialState()
+        {
+             return null;
+        }
+    }
+}
